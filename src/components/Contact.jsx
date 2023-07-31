9bbcff671aa7b5f1f008e01dc45ac4ade8f207b0
Abdelrahman Hassan Mahmoud <AbdelrahmanHassan888@gmail.com>
2023-07-31 09:44:30 +0300

    deployed wesbite

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <form method='POST' action="https://getform.io/f/25116e9a-c740-4b84-bd25-356338adf9f0" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>Submit the form below or shoot me an email - abdelrahmanhassanmahmoud888@gmail.com</p>
+                <p className='text-gray-300 py-4'>Submit the form below or shoot me an email - abdelrahamanhassan01@gmail.com</p>
             </div>
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
